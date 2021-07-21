b0e41647530ac6d0e8a98b41f11eb0f543a9c4ce
Serswii <yegjujlb3>
2021-07-21 15:12:21 +0300

    symfony

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,16 +4,26 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController
+class DefaultController extends AbstractController
 {
 
 
     public function index()
     {
+       /**
+        * @Route("/", name="homepage")
+        */
         $number = random_int(0, 100);
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return new Response(<<<EOF
+<html>
+ <body>
+   <p>Сайт работает</p>
+ </body>
+</html>
+EOF
+ );
     }}
